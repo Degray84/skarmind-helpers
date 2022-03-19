f1c61ed0faa46893197e8c5dcda9dac3e475f45d
Katser <gray84@tut.by>
2022-03-19 17:31:35 +0300

    feat: update structure of files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import Animations from './props.animations.js'
 import Sizes from './props.sizes.js'
 import Colors from './props.colors.js'
-import ColorsHSL from './props.colors-hsl.js'
 import Fonts from './props.fonts.js'
 import Borders from './props.borders.js'
 import Aspects from './props.aspects.js'
 import Easings from './props.easing.js'
-import Gradients from './props.gradients.js'
 import {StaticShadows} from './props.shadows.js'
-import SVG from './props.svg.js'
 import Zindex from './props.zindex.js'
 
 const camelize = text => {
@@ -28,13 +25,10 @@ const OpenProps = mapToObjectNotation({
   ...Animations,
   ...Sizes,
   ...Colors,
-  ...ColorsHSL,
   ...Fonts,
   ...Borders,
   ...Aspects,
   ...Easings,
-  ...SVG,
-  ...Gradients,
   ...StaticShadows,
   ...Zindex,
 })
--- src/props.colors.js
+++ src/props.colors.js
@@ -1,4 +1,3 @@
-/* generated mostly with props.colors.src.js */
 export const Gray = {
   '--gray-0': '#f8f9fa',
   '--gray-1': '#f1f3f5',
@@ -167,44 +166,6 @@ export const Orange = {
   '--orange-8': '#e8590c',
   '--orange-9': '#d9480f',
 }
-
-/* 
-https://codepen.io/argyleink/pen/VwrKRrY?
-* {
-  --gray: #adb5bd;
-  --red: #ff6b6b;
-  --pink: #f06595;
-  --grape: #cc5de8;
-  --violet: #f3f0ff;
-  --indigo: #5c7cfa;
-  --blue: #339af0;
-  --cyan: #22b8cf;
-  --teal: #20c997;
-  --green: #51cf66;
-  --lime: #94d82d;
-  --yellow: #fcc419;
-  --orange: #ff922b;
-
-  --base: var(--indigo);
-
-  --color-0:  lch(from var(--base) 98% 10 h);
-  --color-1:  lch(from var(--base) 93% 20 h);
-  --color-2:  lch(from var(--base) 85% 40 h);
-  --color-3:  lch(from var(--base) 75% 46 h);
-  --color-4:  lch(from var(--base) 66% 51 h);
-  --color-5:  lch(from var(--base) 61% 52 h);
-  --color-6:  lch(from var(--base) 55% 57 h);
-  --color-7:  lch(from var(--base) 49% 58 h);
-  --color-8:  lch(from var(--base) 43% 55 h);
-  --color-9:  lch(from var(--base) 39% 48 h);
-  --color-10: lch(from var(--base) 31% 48 h);
-  --color-11: lch(from var(--base) 25% 48 h);
-  --color-12: lch(from var(--base) 15% 40 h);
-  --color-13: lch(from var(--base) 10% 30 h);
-  --color-14: lch(from var(--base) 5% 20 h);
-  --color-15: lch(from var(--base) 1% 5 h);
-} 
-*/
 
 const Colors = {
   ...Gray,
